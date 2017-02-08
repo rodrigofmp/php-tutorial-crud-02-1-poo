c3b39d3743bf5032f29580b850312927b87bf997
Rodrigo Petters <rodrigo.petters@inovan.net>
2017-02-08 21:56:44 -0200

    Implementação do upload de arquivos

--- dev/script.sql
+++ dev/script.sql
@@ -45,3 +45,6 @@ INSERT INTO `categories` (`id`, `name`, `created`, `modified`) VALUES
 (1, 'Fashion', '2014-06-01 00:35:07', '2014-05-30 17:34:33'),
 (2, 'Electronics', '2014-06-01 00:35:07', '2014-05-30 17:34:33'),
 (3, 'Motors', '2014-06-01 00:35:07', '2014-05-30 17:34:54');
+
+-- Incluir coluna image
+ALTER TABLE `products` ADD COLUMN `image` varchar(250);
