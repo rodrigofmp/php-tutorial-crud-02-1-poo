75f62a911c373d47c0cce3026b2f3f4239e459e9
Rodrigo Petters <rodrigo.petters@inovan.net>
2017-02-08 21:55:49 -0200

    Adicionando paginação

--- search.php
+++ search.php
@@ -1,35 +1,37 @@
 <?php
 // core.php holds pagination variables
 include_once 'config/core.php';
-
+ 
 // include database and object files
 include_once 'config/database.php';
 include_once 'objects/product.php';
 include_once 'objects/category.php';
  
-// instantiate database and objects
+// instantiate database and product object
 $database = new Database();
 $db = $database->getConnection();
  
 $product = new Product($db);
 $category = new Category($db);
-
-// set page header
-$page_title = "Read Products";
+ 
+// get search term
+$search_term=isset($_GET['s']) ? $_GET['s'] : '';
+ 
+$page_title = "You searched for \"{$search_term}\"";
 include_once "header.php";
  
 // query products
-$stmt = $product->readAll($from_record_num, $records_per_page);
+$stmt = $product->search($search_term, $from_record_num, $records_per_page);
  
 // specify the page where paging is used
-$page_url = "index.php?";
+$page_url="search.php?s={$search_term}&";
  
 // count total rows - used for pagination
-$total_rows=$product->countAll();
+$total_rows=$product->countAll_BySearch($search_term);
  
 // read_template.php controls how the product list will be rendered
 include_once "read_template.php";
  
-// set page footer
+// footer.php holds our javascript and closing html tags
 include_once "footer.php";
 ?>
